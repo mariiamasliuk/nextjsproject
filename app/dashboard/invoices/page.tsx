90f3f5c1e918ca9fed652abf708df755a8ff6c1a
Mariia Masliuk <mariia_masliuk@epam.com>
2025-05-05 15:25:55 +0300

    create nextjs project

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Page() {
+  return (
+    <main className="flex min-h-screen flex-col p-6">
+      <h1>Invoices</h1>
+    </main>
+  );
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Page() {
+  return (
+    <main className="flex min-h-screen flex-col p-6">
+      <h1>Helllo</h1>
+    </main>
+  );
+}
