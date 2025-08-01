2e19b2e0161d2ba7ddf9121f349cfb8311bba689
Mariia Masliuk <mariia_masliuk@epam.com>
2025-08-01 13:24:33 +0300

    test

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -42,18 +42,26 @@ const { customerId, amount, status } = UpdateInvoice.parse({
 });
 
 const amountInCents = amount * 100;
-
-await sql`
+try{
+    await sql`
     UPDATE invoices
     SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-    WHERE id = ${id}
-`;
+    WHERE id = ${id}`;
+}
+catch(err){
+    console.error(err);
+}
 
 revalidatePath('/dashboard/invoices');
 redirect('/dashboard/invoices');
+
 }
 
 export async function deleteInvoice(id: string) {
-    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    try {
+        await sql`DELETE FROM invoices WHERE id = ${id}`;
+    } catch (error) {
+        console.error(error);        
+    }
     revalidatePath('/dashboard/invoices');
-  }
+}
